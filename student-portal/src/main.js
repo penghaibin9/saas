ee9d48ee116dd270bed9a36cd268465586be16cf
penghaibin9 <penghaibin9@gmail.com>
2026-07-28 10:09:23 +0800

    refactor(academic): install isolated student portal routes

--- student-portal/src/main.js
+++ student-portal/src/main.js
@@ -2,6 +2,8 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
+import { installAcademicRoutes } from './router/academicRoutes'
 import './styles.css'
 
+installAcademicRoutes(router)
 createApp(App).use(createPinia()).use(router).mount('#app')
