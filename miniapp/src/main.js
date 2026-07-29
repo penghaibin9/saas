34b264e40e48c5a9e2cf1f2657fe16fe584cf363
penghaibin9 <penghaibin9@gmail.com>
2026-07-29 10:51:29 +0800

    fix(miniapp): install academic session plugin without dropping main affairs contracts

--- miniapp/src/main.js
+++ miniapp/src/main.js
@@ -2,6 +2,7 @@ import { createSSRApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
 import { academicSessionPlugin } from '@/stores/sessionAcademicPlugin'
+import './services/affairsAllowedActions'
 
 // uni-app + Vue3 标准入口。仅本小程序工程使用。
 export function createApp() {
