0f8f413dfa760593a4e5d49ca14efbd8e59591f0
penghaibin9 <penghaibin9@gmail.com>
2026-07-29 10:45:39 +0800

    fix(student-portal): install academic routes without dropping main plugins

--- student-portal/src/main.js
+++ student-portal/src/main.js
@@ -3,6 +3,7 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 import { installAcademicRoutes } from './router/academicRoutes'
+import './services/affairsAllowedActions'
 import './styles.css'
 
 installAcademicRoutes(router)
