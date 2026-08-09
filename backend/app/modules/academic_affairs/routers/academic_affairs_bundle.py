8f2c1a04eff7450c7a0d32cdfd45823ef35103ee
penghaibin9 <penghaibin9@gmail.com>
2026-08-09 13:42:12 +0800

    feat(stage-c3): register immutable archive correction extension

--- backend/app/modules/academic_affairs/routers/academic_affairs_bundle.py
+++ backend/app/modules/academic_affairs/routers/academic_affairs_bundle.py
@@ -15,6 +15,7 @@ from . import academic_affairs as base_router
 
 _EXTENSION_ROUTER_MODULES = (
     "academic_file_exchange_router",
+    "archive_correction_router",
     "dashboard_readiness_router",
     "dynamic_grade_router",
     "exam_incident_closure_router",
