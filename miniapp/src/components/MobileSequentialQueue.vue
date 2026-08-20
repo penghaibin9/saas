136c090bf2e73e6c3b7af2eb127c5454c7cb68ee
penghaibin9 <penghaibin9@gmail.com>
2026-08-20 21:13:21 +0800

    fix(miniapp): keep sequential queue easycom compatibility

---
+++ miniapp/src/components/MobileSequentialQueue.vue
@@ -0,0 +1,21 @@
+<template>
+  <component :is="queueComponent" v-bind="$attrs">
+    <template #default="slotProps">
+      <slot v-bind="slotProps" />
+    </template>
+  </component>
+</template>
+
+<script>
+import TeacherMobileSequentialQueue from './teacher/MobileSequentialQueue.vue'
+
+export default {
+  name: 'MobileSequentialQueueCompat',
+  inheritAttrs: false,
+  computed: {
+    queueComponent() {
+      return TeacherMobileSequentialQueue
+    }
+  }
+}
+</script>
