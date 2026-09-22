38ee211958aea8f1dbc0c5c3fa4a1f6836f41e9e
彭大哥 <penghaibin9@gmail.com>
2026-09-22 11:49:07 +0800

    fix(academic): route legacy academic root to teacher today content

--- frontend/src/modules/academicAffairs/views/AaDashboardView.vue
+++ frontend/src/modules/academicAffairs/views/AaDashboardView.vue
@@ -1,5 +1,6 @@
 <template>
-  <AaLeadershipWallView v-if="$route.query.wall === '1'" :ctx="ctx" />
+  <AaTeacherTodayView v-if="isAcademicTeacher" :ctx="ctx" />
+  <AaLeadershipWallView v-else-if="$route.query.wall === '1'" :ctx="ctx" />
   <AaOverviewWorkspace v-else-if="activePanel === 'todos'" key="todos" :ctx="ctx" mode="todos" />
   <AaAcademicProgressWorkspace v-else-if="activePanel === 'academicProgress'" :ctx="ctx" />
   <AaTodayTeachingWorkspace v-else-if="activePanel === 'todayTeaching'" :ctx="ctx" />
@@ -13,6 +14,7 @@ import { dashboardPanel } from '../config/dashboardPanels'
 export default {
   name: 'AaDashboardView',
   components: {
+    AaTeacherTodayView: defineAsyncComponent(() => import('./AaTeacherTodayView.vue')),
     AaLeadershipWallView: defineAsyncComponent(() => import('./AaLeadershipWallView.vue')),
     AaDashboardPanelWorkspace: defineAsyncComponent(() => import('../components/AaDashboardPanelWorkspace.vue')),
     AaOverviewWorkspace: defineAsyncComponent(() => import('../components/AaOverviewWorkspace.vue')),
@@ -20,6 +22,11 @@ export default {
     AaTodayTeachingWorkspace: defineAsyncComponent(() => import('../components/AaTodayTeachingWorkspace.vue'))
   },
   props: { ctx: { type: Object, required: true } },
-  computed: { activePanel() { return dashboardPanel(this.$route.query.panel) } }
+  computed: {
+    isAcademicTeacher() {
+      return String(this.ctx?.currentRole?.roleCode || this.ctx?.currentRole?.roleType || '').toUpperCase() === 'ACADEMIC_TEACHER'
+    },
+    activePanel() { return dashboardPanel(this.$route.query.panel) }
+  }
 }
 </script>
